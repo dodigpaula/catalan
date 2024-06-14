9fb3cf67b2f827627cb59f4465a0319d31967c88
dodigpaula <59296480+dodigpaula@users.noreply.github.com>
2024-06-14 18:35:49 +0200

    Update Project.lean

--- Project.lean
+++ Project.lean
@@ -1,6 +1,6 @@
 -- This module serves as the root of the `Project` library.-/
 -- Import modules here that should be built as part of the library.-/
 
-import «Project».Basic
+import «catalan».LargeTasks
 import Mathlib
 
